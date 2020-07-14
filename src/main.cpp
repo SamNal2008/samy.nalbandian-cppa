c2e49c650ab409c9e0b744fa2e061c9f5272127e
samy.nalbandian <samy.nalbandian@epita.fr>
2020-07-14 06:29:06 -0700

    [CMAKE] pybind11

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include <Python.h>
 #include <iostream>
+#include <mln/core/se/private/se_facade.hpp>
+#include <mln/core/box.hpp>
+#include <mln/core/se/disc.hpp>
+#include <memory>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 int main(void)
 {
-  std::cout << "Hello world\n";
-  return 1;
+    std::cout << "Hello world\n";
+    return 1;
 }
